68a432a45bf6914a58befaada323f29aa88c9c42
ron0401 <51016256+ron0401@users.noreply.github.com>
2021-05-07 00:03:23 +0900

    プロジェクト ファイルを追加します。

---
+++ NMEAParserNET/GGA.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using CoordinateNET;
+
+namespace NMEAParserNET
+{
+    public class GGA : Sentence
+    {
+        public GGATime Time { get; set; }
+
+
+        public class GGATime
+        {
+            public uint Hour { get; set; }
+            public uint Minute { get; set; }
+            public uint Second { get; set; }
+        }
+        public Latitude Latitude { get; set; }
+        public Longitude Longitude { get; set; }
+
+        public enum TypeOfSPS
+        {
+            NotSpecific = 0, SPS = 1, DGPS = 2
+        }
+        public uint NumberOfSatellites { get; set; }
+        /// <summary>
+        /// Horizontal accuracy reduction rate
+        /// </summary>
+        public double HARR { get; set; }
+        public double AntennaHeight { get; set; }
+        public double GeoidHeight { get; set; }
+
+        public override string Message { get { return "GGA"; } }
+
+        public override string AAA => throw new NotImplementedException();
+    }
+}
---
+++ NMEAParserNET/Sentence.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMEAParserNET
+{
+
+    /// <summary>
+    /// NMEA Sentence Super Class.
+    /// </summary>
+    public abstract class Sentence
+    {
+        //public Sentence(string sentense) 
+        //{
+
+        //}
+        public abstract string Message { get; }
+        private string _SentenceString;
+        public string SentenceString
+        {
+            get
+            {
+                return _SentenceString;
+            }
+            set
+            {
+                if (value.Length < 6)
+                {
+                    throw new Exception();
+                }
+                if (value.Substring(value.Length - 2) != "\r\n" || value.Substring(0, 1) != "$" || value.Substring(value.Length - 5).Substring(0,2) != "*")
+                {
+                    throw new Exception();
+                }
+                _SentenceString = value;
+            }
+        }
+        private string getDataString() 
+        {
+            int end = _SentenceString.IndexOf('*');
+            int start = _SentenceString.IndexOf('$');
+            return _SentenceString.Substring(start + 1, end - start - 1);
+        }
+        private string getCheckSum(string str)
+        {
+            throw new Exception();
+        }
+        public Byte CheckSum 
+        {
+            get 
+            {
+                
+                throw new Exception();
+            } 
+        }
+
+        //public static byte getNEMAcheckSum(String data, int startPos, int endPos)
+        //{
+        //    byte checkSum = 0;
+        //    for (int i = startPos; i < endPos; i++)
+        //    {
+        //        checkSum ^= (byte)data.charAt(i);
+        //    }
+        //    return checkSum;
+        //}
+    }
+}
