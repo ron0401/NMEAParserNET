8c78bd3327de696c36369c57d98e5950de7097b3
ron0401 <51016256+ron0401@users.noreply.github.com>
2021-05-09 14:36:24 +0900

    init

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -7,13 +7,15 @@ namespace Sample
     {
         static void Main(string[] args)
         {
-            var gga = new GGA("GNGGA,035817.30,3444.3418019,N,13535.1207337,E,5,12,2.59,25.572,M,34.179,M,1.3,0000*50");
+            string sentence = "GNGGA,035817.30,3444.3418019,N,13535.1207337,E,5,12,2.59,25.572,M,34.179,M,1.3,0000*50";
+            var gga = new GGA(sentence);
             var op = new System.Text.Json.JsonSerializerOptions() { WriteIndented = true, IgnoreReadOnlyProperties = true };
             op.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
             Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(gga, op));
-            gga.Talker = "GP";
-            gga.AntennaHeight = 1.443;
-            Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(gga, op));
+
+            var type = NMEAParserNET.Parser.JudgeMsgType(sentence);
+            Console.WriteLine(type.ToString());
+
         }
-}
+    }   
 }
